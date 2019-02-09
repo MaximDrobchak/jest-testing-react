d14c11ffeae7c5729bc7bfc559aa623b933906dc
MaksimDrobchak <drobchak1988@gmail.com>
2019-02-09 10:49:10 +0200

    ready my ferst hook testing. I like dhis

--- src/App.spec.js
+++ src/App.spec.js
@@ -1,5 +1,13 @@
+import { doMutationCount } from './App';
 describe('Local State', () => {
-  it('should increment the countere', () => {});
+  it('should increment the countere', () => {
+    function test (x){
+      return x;
+    }
+    const value = 1;
+
+    expect(doMutationCount(test, value)).to.equal(value);
+  });
 
   it('should decrement the countere', () => {});
 });
