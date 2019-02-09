4f5e3ef411d0ada0e7006693544fd76bfefb938a
Maxim Drobchak <drobchak1988@gmail.com>
2019-02-09 09:43:21 +0200

    minimal test ready

--- src/App.spec.js
+++ src/App.spec.js
@@ -1,5 +1,5 @@
 describe('Local State', () => {
-  it('should increment the counter in state', () => {});
+  it('should increment the countere', () => {});
 
-  it('should decrement the counter in state', () => {});
+  it('should decrement the countere', () => {});
 });
